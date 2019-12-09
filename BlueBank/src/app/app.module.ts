878ec4da4b9b1f7a1f032f9029a51eea6fe8166e
bossbuwi <mjmananghaya@gmail.com>
2019-12-10 02:13:49 +0800

    Navbar + Sidebar Works

--- BlueBank/src/app/app.module.ts
+++ BlueBank/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layouts/header/header.component';
+import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
+import { WorkspaceComponent } from './components/layouts/workspace/workspace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
