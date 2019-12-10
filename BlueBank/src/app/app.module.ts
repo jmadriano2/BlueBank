a78265da573cfc820c081230f7475699067c0b6b
bossbuwi <mjmananghaya@gmail.com>
2019-12-10 10:53:07 +0800

    Revamped Navbar
    bitch

--- BlueBank/src/app/app.module.ts
+++ BlueBank/src/app/app.module.ts
@@ -5,23 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/layouts/navbar/navbar.component';
-import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
+// import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
+import { SidenavResponsiveExample } from './components/layouts/sidebar/sidebar.component';
 import { WorkspaceComponent } from './components/layouts/workspace/workspace.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material';
+import {MatListModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {CustomMaterialModule} from './material-module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent,
+    SidenavResponsiveExample,
     WorkspaceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    CustomMaterialModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
