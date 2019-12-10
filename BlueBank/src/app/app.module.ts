fd73d900706dbdd8b28933eccac33161494a00b9
bossbuwi <mjmananghaya@gmail.com>
2019-12-10 08:50:24 +0800

    Navbar Initial

--- BlueBank/src/app/app.module.ts
+++ BlueBank/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
+import { WorkspaceComponent } from './components/layouts/workspace/workspace.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
