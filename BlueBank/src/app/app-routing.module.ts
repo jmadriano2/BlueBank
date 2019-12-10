410d14297c09a8ab54bae2e002e0c1314ba684d7
bossbuwi <mjmananghaya@gmail.com>
2019-12-11 06:55:53 +0800

    Revamped Navbar 1.2.1
    Changed wrong logo on the navbar

--- BlueBank/src/app/app-routing.module.ts
+++ BlueBank/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/layouts/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
